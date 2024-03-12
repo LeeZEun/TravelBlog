abda7a1a066ac28f7f9e3b6cd4f04d9333f2309a
LeeZEun <je990420@ajou.ac.kr>
2024-03-12 14:00:34 +0900

    Chage java version : 11 to 17

---
+++ Backend/src/main/java/com/travelblog/Backend/entity/UserInfo.java
@@ -0,0 +1,18 @@
+package com.travelblog.Backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+public class UserInfo {
+
+    private Long usernumber; //회원번호
+    private String username; //이름
+    private String useremail; //이메일
+    private String userphonenum; //폰번호
+    private String userid; //아이디
+    private String userpassword; //비번
+    private String usernickname; //닉네임
+
+
+
+
+}
