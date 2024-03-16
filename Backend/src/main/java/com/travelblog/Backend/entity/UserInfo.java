9125c5a989b66e8ccf27e1c5453eae9899fe006f
LeeZEun <je990420@ajou.ac.kr>
2024-03-17 00:03:07 +0900

    [Update] UnserInfo

--- Backend/src/main/java/com/travelblog/Backend/entity/UserInfo.java
+++ Backend/src/main/java/com/travelblog/Backend/entity/UserInfo.java
@@ -1,12 +1,16 @@
 package com.travelblog.Backend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import lombok.*;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,16 +21,24 @@ import lombok.Setter;
 @Table(name = "user_info")
 public class UserInfo {
 
-
+    @Id
+    @Column(unique = true)
     private Long usernumber; //회원번호
-    private String username; //이름
-    private String useremail; //이메일
-    private String userphonenum; //폰번호
-    private String userid; //아이디
+
+
+    @Column private String username; //이름
+    @Column(unique = true) private String useremail; //이메일
+    @Column(unique = true) private String userphonenum; //폰번호
+    @Column(unique = true) private String userid; //아이디
     private String userpassword; //비번
-    private String usernickname; //닉네임
+    @Column(unique = true) private String usernickname; //닉네임
 
+    private boolean delete_yn; //탈퇴 true, 미탈퇴 false
+    private LocalDateTime created_at; //가입일시
+    @Column private LocalDateTime recent_access; //최근 로그인 일시
+    @Column private LocalDateTime modified_data; //회원정보 수정 일시
 
-
+    @Column private Integer alarm; //알림 기능 on 1 & off 0
+    @Column private Long login_num; //로그인 횟수[추가]
 
 }
