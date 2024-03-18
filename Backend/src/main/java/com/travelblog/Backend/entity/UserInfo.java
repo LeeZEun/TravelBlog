ab01faf699207adb5683504d5d541cf625587410
LeeZEun <je990420@ajou.ac.kr>
2024-03-18 12:41:52 +0900

    [Update] Make UserJointRequestDto

--- Backend/src/main/java/com/travelblog/Backend/entity/UserInfo.java
+++ Backend/src/main/java/com/travelblog/Backend/entity/UserInfo.java
@@ -33,12 +33,12 @@ public class UserInfo {
     @Column private String userpassword; //비번
     @Column(unique = true) private String usernickname; //닉네임
 
-    @Column private boolean delete_yn; //탈퇴 true, 미탈퇴 false
-    @Column private LocalDateTime created_at; //가입일시
-    @Column private LocalDateTime recent_access; //최근 로그인 일시
-    @Column private LocalDateTime modified_data; //회원정보 수정 일시
+    @Column private boolean deleteyn; //탈퇴 true, 미탈퇴 false
+    @Column private LocalDateTime createdat; //가입일시
+    @Column private LocalDateTime recentaccess; //최근 로그인 일시
+    @Column private LocalDateTime modifieddata; //회원정보 수정 일시
 
     @Column private Integer alarm; //알림 기능 on 1 & off 0
-    @Column private Long login_num; //로그인 횟수[추가]
+    @Column private Long loginnumber; //로그인 횟수[추가]
 
 }
